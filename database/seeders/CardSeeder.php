d0de981b6190b483b9bdc412a4bb67b694546bdf
naomi.akagi <n.akagi@lexues.co.jp>
2023-10-06 07:34:18 +0000

    make database add seeder

---
+++ database/seeders/CardSeeder.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CardSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 1,
+            'image_path' => 'img/cards/fire1_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 1,
+            'image_path' => 'img/cards/fire1_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 1,
+            'image_path' => 'img/cards/fire1_3.png'
+        ]);
+        
+        
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 3,
+            'image_path' => 'img/cards/fire3_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 3,
+            'image_path' => 'img/cards/fire3_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 3,
+            'image_path' => 'img/cards/fire3_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 5,
+            'image_path' => 'img/cards/fire5_1.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 5,
+            'image_path' => 'img/cards/fire5_2.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 5,
+            'image_path' => 'img/cards/fire5_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 7,
+            'image_path' => 'img/cards/fire7_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 7,
+            'image_path' => 'img/cards/fire7_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'fire',
+            'score' => 7,
+            'image_path' => 'img/cards/fire7_3.png'
+        ]);
+        
+        
+        
+        
+        
+        
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 1,
+            'image_path' => 'img/cards/water1_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 1,
+            'image_path' => 'img/cards/water1_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 1,
+            'image_path' => 'img/cards/water1_3.png'
+        ]);
+        
+        
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 3,
+            'image_path' => 'img/cards/water3_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 3,
+            'image_path' => 'img/cards/water3_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 3,
+            'image_path' => 'img/cards/water3_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 5,
+            'image_path' => 'img/cards/water5_1.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 5,
+            'image_path' => 'img/cards/water5_2.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 5,
+            'image_path' => 'img/cards/water5_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 7,
+            'image_path' => 'img/cards/water7_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 7,
+            'image_path' => 'img/cards/water7_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'water',
+            'score' => 7,
+            'image_path' => 'img/cards/water7_3.png'
+        ]);
+        
+        
+        
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 1,
+            'image_path' => 'img/cards/green1_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 1,
+            'image_path' => 'img/cards/green1_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 1,
+            'image_path' => 'img/cards/green1_3.png'
+        ]);
+        
+        
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 3,
+            'image_path' => 'img/cards/green3_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 3,
+            'image_path' => 'img/cards/green3_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 3,
+            'image_path' => 'img/cards/green3_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 5,
+            'image_path' => 'img/cards/green5_1.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 5,
+            'image_path' => 'img/cards/green5_2.png'
+        ]);
+         DB::table('cards')->insert([
+            'attribute' =>'green',
+            'score' => 5,
+            'image_path' => 'img/cards/green5_3.png'
+        ]);
+        
+        
+         DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 7,
+            'image_path' => 'img/cards/green7_1.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 7,
+            'image_path' => 'img/cards/green7_2.png'
+        ]);
+        DB::table('cards')->insert([
+            'attribute' => 'green',
+            'score' => 7,
+            'image_path' => 'img/cards/green7_3.png'
+        ]);
+    }
+}
